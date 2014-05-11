fe23a4feffab148855a2ccf401c8c0c26ae602f0
sebafonte <sebafonte@gmail.com>
2014-05-11 14:28:31 -0300

    + Task creator model

--- Source/Model/Tasks and search distribution/task-creator.lisp
+++ Source/Model/Tasks and search distribution/task-creator.lisp
@@ -1,6 +1,54 @@
 
+(defclass range-generator (base-model)
+  ())
+
+
+(defmethod variations ((o range-generator))
+  (error "Subclass responsibility"))
+
+(defmethod abstractp ((o (eql 'range-generator)))
+  "Answer whether <o> is abstract."
+  t)
+
+(defmethod variations-from-description (description)
+  (let ((objects (concrete-subclasses 'range-generator)))
+    (dolist (i objects)
+      (multiple-value-bind (value property)
+          (try-parse-from i description)
+        (when property
+          (return-from variations-from-description (values value property)))))
+    (error "Invalid range object")))
+
+
+(defclass from-to-range (range-generator)
+  ((from :initarg :from :accessor from)
+   (to :initarg :to :accessor to)
+   (step-by :initarg :step-by :accessor step-by)))
+
+
+(defmethod try-parse-from ((o (eql 'from-to-range)) value)
+  nil)
+
+(defmethod variations ((o from-to-range))
+  ())
+
+
+(defclass values-range (range-generator)
+  ((elements :initarg :elements :accessor elements)))
+
+
+(defmethod try-parse-from ((o (eql 'values-range)) value)
+  (if (equal (cadr value) :values)
+      (values (cddr value) (car value))
+    (values nil nil)))
+
+(defmethod variations ((o values-range))
+  elements)
+
+
 (defclass task-creator (object-with-properties)
   ((name :initarg :name :initform nil :accessor name)
+   (task-name :initarg :task-name :accessor task-name)
    (variations :initarg :variations :accessor variations)))
 
 
@@ -10,43 +58,84 @@
    o 
    (:name 'name :label "Name" :accessor-type 'accessor-accessor-type 
     :data-type 'string :default-value "Task creator" :editor 'text-editor)
+   (:name 'task-name :label "Task name" :accessor-type 'accessor-accessor-type 
+    :data-type 'string :default-value "Task" :editor 'text-editor)
    (:name 'variations :label "Variations" :accessor-type 'accessor-accessor-type 
     :data-type 'list :default-value nil :editor 'list-editor)))
 
-(defmethod create-tasks ((creator task-creator) (task search-task))
-  "Answer the search subtasks for <builder>."
-  (let ((result)
-        (old-process (process task)))
+(defmethod initialize-instance :after ((o task-creator) &rest args &key variations-description)
+  "Initialize <o>."
+  (setf (variations o) (combine-variations o variations-description)))
+
+;; #TODO:
+(defmethod combine-variations ((o task-creator) variations-description)
+  (let ((result))
+    (dolist (i variations-description)
+      (multiple-value-bind (values property)
+          (variations-from-description i)
+        (appendf result (list (list property values)))))
+    result))
+
+(defmethod create-tasks-on ((creator task-creator) (task search-task))
+  (let* ((old-process (process task))
+         (variations (variations creator))
+         (properties (get-variation-properties variations))
+         (values (get-variation-values variations))
+         (combinations (get-variation-combinations values))
+         (result))
     ;; #TODO: Check if necessary
     (setf (process task) nil)
-    (dolist (i (variation-values creator))
-      (let ((new-task (copy-cyclic new-task)))
-        (apply-variation-values creator new-task i)
+    (dolist (i combinations)
+      (let ((new-task (copy-cyclic task)))
+        (setf (name new-task) (format nil "~A~A.task" (task-name creator) (length result)))
+        (apply-variation creator new-task i properties)
         (appendf result (list (copy-cyclic new-task)))))
     ;; #TODO: Check if necessary
     (setf (process task) old-process)
     result))
 
-(defmethod variation-values ((creator task-creator))
-  nil)
+(defmethod apply-variation ((creator task-creator) new-task variation properties)
+  (dolist (i properties)
+    ;; Go to last property of the chain
+    (let ((final-object new-task))
+      (dolist (j i)
+        (if (eql j (car (last i)))
+            ;; Set value for property
+            (apply-chained-property final-object j variation)
+          ;; Move to next object
+          (setf final-object (get-value-for-property-named new-task j)))))))
 
-(defmethod apply-variation-values ((creator task-creator) new-task variation)
-  nil)
+(defmethod apply-chained-property (object property value)
+  (if (symbolp property)
+      (set-value-for-property-named object property value)
+    (if (functionp property)
+        (apply property (list object value))
+      (error "Bad property"))))
 
-(defmethod create-tasks-on ((creator task-creator) (task search-task) path)
-  (let ((tasks (tasks creator)))
-    (dolist (i tasks)
-      (save-task-on path i))))
+(defun get-variation-properties (variations)
+  (mapcar 'car variations))
 
-(defmethod create-tasks-on ((creator task-creator) (task search-task) path)
-  nil)
+(defun get-variation-values (variations)
+  (mapcar 'cadr variations))
+
+(defun get-variation-combinations (variations)
+  (first variations))
+
+(defun save-task-on (creator tasks path)
+  (dolist (i tasks)
+    (let ((file-name (merge-pathnames (name i) path)))
+      (if (probe-file file-name) (delete-file file-name))
+      (save-source-description task file-name))))
+
 
 #|
-
 ;; Population size test
-(make-instance 'task-creator :variations '((population-size (:from 5 :to 500 :step 10))))
+(setf creator (make-instance 'task-creator :variations-description '(((algorithm population-size) . (:from 5 :to 500 :step 10)))))
+(setf task (make-instance 'search-task))
+(save-task-on creator (create-tasks-on creator task) "d:\\temp\\tasks\\")
 
-;; Population size 
-(make-instance 'task-creator :variations '((population-size (:values 5 10 15 20 25))))
-
+;; Population size
+(setf creator (make-instance 'task-creator :variations-description '(((algorithm population-size) . (:values 5 10 15 20 25)))))
+(setf task (make-instance 'search-task))
+(save-task-on creator (create-tasks-on creator task) "d:\\temp\\tasks\\")
 |#
