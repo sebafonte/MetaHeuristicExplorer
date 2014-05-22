cf10cc63e16bd0c4ed2882a03016d11bef2c402f
sebafonte <sebafonte@gmail.com>
2014-05-21 22:20:37 -0300

    * Adjustment

--- Source/property-function.lisp
+++
@@ -1,9 +0,0 @@
-(defclass property-function ()
-  ((lisp-function :initarg :lisp-function :initform nil :accessor lisp-function)
-   (code :initarg :code :initform 'nil :accessor code)))
-
-
-(defmethod lisp-function ((object property-function))
-  (if (null function)
-      (setf (function object) 
-            (compile (code object)))))
