bc5998677eab4dd331bb89a6f99229a6b40d0d93
sebafonte <sebafonte@gmail.com>
2014-06-08 03:34:24 -0300

    + Launcher runtime commands

---
+++ Runtime/LaunchClient.cmd
@@ -0,0 +1,1 @@
+start "GPExplorer" GE025-development.exe
---
+++ Runtime/LaunchServer.cmd
@@ -0,0 +1,6 @@
+start "GPExplorer1" GE025-silent-development.exe :port 20001
+start "GPExplorer2" GE025-silent-development.exe :port 20002
+start "GPExplorer3" GE025-silent-development.exe :port 20003
+start "GPExplorer4" GE025-silent-development.exe :port 20004
+start "GPExplorer5" GE025-silent-development.exe :port 20005
+start "GPExplorer6" GE025-silent-development.exe :port 20006
