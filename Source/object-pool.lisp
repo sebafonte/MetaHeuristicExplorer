cf14d3ae3e12861082fb36c80514cdfe6e2cf79a
sebafonte <sebafonte@gmail.com>
2016-09-02 01:43:38 -0300

    + object-pool.lisp functionality

--- Source/object-pool.lisp
+++ Source/object-pool.lisp
@@ -1,21 +1,89 @@
 
 (defclass object-pool ()
-  ((registry :initform (make-hash-table) :accessor registry)))
+  ((registry :initform (make-hash-table :test 'string=) :accessor registry)
+   (current-count :initform 0 :accessor current-count)
+   (max-count :initform 5 :initarg :max-count :accessor max-count)))
 
 
+;; Interface
 (defmethod clear ((o object-pool))
-  (clear-hash (registry o)))
+  (clrhash (registry o))
+  (setf (current-count o) 0))
 
-(defmethod get-object ((o object-pool) key)
-  (get-hash (registry o) key))
+(defmethod get-pool-object ((o object-pool) object)
+  (get-object-key o (get-key object)))
 
+(defmethod get-pool-object-key ((o object-pool) key)
+  (get-object-key o key))
+
+(defmethod put-pool-object ((o object-pool) object)
+  (let* ((key (get-key object))
+         (value (get-object-key o key)))
+    (unless value
+      (add-object-on o key object))))
+
+(defmethod put-pool-object-key ((o object-pool) object key)
+  (let ((value (get-object-key o key)))
+    (unless value
+      (add-object-on o key object))))
+
+;; Internal
 (defmethod set-object ((o object-pool) key value)
-  (setf (get-hash (registry o) key) value))
+  (setf (gethash key (registry o)) value))
 
-(defmacro check-object (o key &rest body)
-  `(let ((value (get-object key)))
-     (unless value
-       (setf value (progn ,@body))
-       (set-object o key value))
-     value))
+(defmethod add-object-on ((o object-pool) key object)
+  (when (>= (current-count o) (max-count o)) 
+    ;(print "clear")
+    (clear o)
+    (drop-object o key))
+  (setf value object)
+  (set-object o key value)
+  (incf (current-count o)))
+
+(defmethod drop-object ((o object-pool) key)
+  (remhash key (registry o))
+  ;; #NOTE: Assume o was deleted
+  (decf (current-count o)))
+
+(defmethod get-object-key ((o object-pool) key)
+  (gethash key (registry o)))
+
+
+;; Get key functions
+(defmethod get-key ((obj list))
+  (let ((*print-level* 64)
+        (*print-length* 10000000)
+        (result ""))
+    (dolist (i obj)
+      (setf result (format nil "~A-~A" result i)))
+    result))
+
+#|
+;; Test
+(let ((pool (make-instance 'object-pool)))
+  ;(check 0)
+  (put-pool-object pool '(1 2 3))
+  ;(check 1)
+  (put-pool-object pool '(1 2 3))
+  ;(check 2)
+  )
+
+(let ((pool (make-instance 'object-pool)))
+  (put-pool-object pool '(1 2 3))
+  (put-pool-object pool '(1 2 3))
+  ;(check 1)
+  )
+
+(let ((pool (make-instance 'object-pool :max-count 1)))
+  (put-pool-object pool '(1 2 3))
+  ;(check 1)
+  (put-pool-object pool '(1 2 3))
+  ;(check 1)
+  )
+
+(let ((pool (make-instance 'object-pool)))
+   (put-pool-object pool '(1 2 3))
+   (clear pool)
+   ;(check 0)
+|#
 
